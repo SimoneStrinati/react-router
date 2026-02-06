4e94b1135079c81aba6462214fe1f9deb9f59926
simonestrinati <simone.strinati@outlook.it>
2026-02-06 18:35:14 +0100

    import router e creazione pagine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import HomePage from "./pages/Homepage";
+import ChiSiamo from './pages/ChiSiamo';
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  const apiUrl = 'https://fakestoreapi.com/products';
+
+  axios.get(apiUrl)
+    .then(response => console.log(response.data));
+
   return (
     <>
-    <h1>Hello React-Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={HomePage} />
+          <Route path='/chisiamo' Component={ChiSiamo} />
+        </Routes>
+      </BrowserRouter>
+
+
+
     </>
   )
 }
---
+++ src/pages/ChiSiamo.jsx
@@ -0,0 +1,5 @@
+function ChiSiamo () {
+    return <div>Chi Siamo</div>
+}
+
+export default ChiSiamo;
